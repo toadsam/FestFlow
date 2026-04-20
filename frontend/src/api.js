b5c7e308a8b3f39d7a5d9831570e98977bdc085c
toadsam <toadsam@naver.com>
2026-04-20 21:58:14 +0900

    1

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import { getAccessToken } from "./utils/auth";
 
-const API_BASE = "http://localhost:8080/api";
+const API_BASE = (
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api"
+).replace(/\/$/, "");
 
 function withAuth(headers = {}) {
   const token = getAccessToken();
